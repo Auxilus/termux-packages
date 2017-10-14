169f5b4badcf75d285ca142ba9fad108b2230bd3
Auxilus <raghavsphadke@gmail.com>
2017-10-14 19:02:31 +0530

    Add package aircrack-ng

---
+++ packages/aircrack-ng/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org
+TERMUX_PKG_DESCRIPTION="an 802.11 WEP and WPA-PSK keys cracking program"
+# TERMUX_PKG_VERSION=  not required 
+TERMUX_PKG_SRCURL=https://Auxilus.github.io/aircrack-ng.tar.gz
+TERMUX_PKG_SHA256=9992129ce2d3b5297e422e529a8230a6a449df9fec2edd04636d206c6851e7d5
+TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_post_configure () {
+    sed 's|/usr/local|/data/data/com.termux/files/usr|g' -i common.mak 
+} 
